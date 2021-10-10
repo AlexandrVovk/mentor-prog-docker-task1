aefc848b55c3c709ea5175b23bc770f2250f50ee
oleksandr_vovk <oleksandr_vovk@epam.com>
2021-10-10 19:43:21 +0300

    add mysql-connect

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+  host="172.17.0.2",
+  user="root",
+  password="my-secret-pw",
+  database="app"
+)
+
+@app.route("/")
+def index():
+    # return "Hello World!"
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT text FROM app_table ORDER BY ID DESC LIMIT 1;")
+    myresult = mycursor.fetchall()
+
+    return myresult[0][0]
